8adb1d67556c07fa41b68f6b30572afe90549346
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2022-10-22 17:57:32 +0530

    commit

--- src/main/main.js
+++ src/main/main.js
@@ -22,6 +22,67 @@ const items = [
     { name: "robin", image: "../assets/robin.png" },
     { name: "sanji", image: "../assets/sanji.png" },
     { name: "usopp", image: "../assets/usopp.png" },
-    { name: "zoro", image: "../assets/zoro.png" }
+    { name: "zoro", image: "../assets/zoro.png" },
 ];
+// total card
+let cards;
+let interval;
+let firstCard = false;
+let secondCard = false;
+let winCount = 0;
+// auto time generator 
+// 01:29:34  => 01:30:00
+let seconds = 0;
+let minutes = 0;
+const timeGenerator = () => {
+    seconds += 1;
+    if (seconds >= 60) {
+        minutes += 1;
+        seconds = 0;
+    }
+    ;
+    let secondsValue = seconds < 10 ? `0${seconds}` : seconds; // 1-9 => 01 otherwise 11
+    let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
+    time.innerHTML = `<span>Time: </span>${minutesValue}:${secondsValue}`;
+};
+// auto move generator 
+let movesCount = 0;
+const movesCounter = () => {
+    movesCount += 1;
+    moves.innerHTML = `<span>Moves: </span>${movesCount}`;
+};
+const generateRandom = (size = 4) => {
+    // temporary array with all values
+    let tempArray = [...items];
+    // original size 
+    size = (size * size) / 2;
+    // result 
+    let resultArray = [];
+    //Random selection 
+    for (let i = 0; i < size; i++) {
+        const randomIndex = Math.floor(Math.random() * tempArray.length);
+        resultArray.push(tempArray[randomIndex]);
+        // bug => avoid duplicate element multiple time 
+        tempArray.splice(randomIndex, 1);
+    }
+    return resultArray;
+};
+const matrixGenerator = (resultArray, size = 4) => {
+    // duplicate elements 
+    resultArray = [...resultArray, ...resultArray];
+    resultArray.sort(() => Math.random() - 0.5);
+    gameContainer.innerHTML = "";
+    for (let i = 0; i < size * size; i++) {
+        gameContainer.innerHTML += `
+        <div class="card-container" data-card-value="${resultArray[i].name}">
+                    <div class="card-container__before">X</div>
+                    <div class="card-container__after">
+                        <img src="${resultArray[i].image}"class="image" alt="images">
+                    </div>
+         </div>
+        `;
+    }
+    gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+};
+matrixGenerator(generateRandom(4), 4);
 //# sourceMappingURL=main.js.map
