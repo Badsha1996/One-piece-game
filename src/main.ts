97e12d3c4113132f3a7a646bf8b79ea6aef50bd4
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2022-10-22 15:42:03 +0530

    image-push

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
+// two buttons 
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
 
+const result = document.getElementById("result");
 
+// status values
+const moves = document.getElementById("moves");
+const time = document.getElementById("time");
 
+// container 
+const controls = document.querySelector(".controls-container");
+const gameContainer = document.querySelector(".main-container__game");
+
+// Image Array for the game 
+const items = [
+    {name:"brook", image: "../assets/brook.png"}
+]
 
 
 
--- src/main/main.js
+++ src/main/main.js
@@ -1,2 +1,16 @@
 "use strict";
+// two buttons 
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const result = document.getElementById("result");
+// status values
+const moves = document.getElementById("moves");
+const time = document.getElementById("time");
+// container 
+const controls = document.querySelector(".controls-container");
+const gameContainer = document.querySelector(".main-container__game");
+// Image Array for the game 
+const items = [
+    { name: "brook", image: "../assets/brook.png" }
+];
 //# sourceMappingURL=main.js.map
