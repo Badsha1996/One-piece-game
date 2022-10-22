dafc818449c3bedacf1428f460603a26d3f2f0e2
Badsha Laskar <43995221+Badsha1996@users.noreply.github.com>
2022-10-22 15:44:37 +0530

    ts-images

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,18 @@ const gameContainer = document.querySelector(".main-container__game");
 
 // Image Array for the game 
 const items = [
-    {name:"brook", image: "../assets/brook.png"}
+    { name: "brook", image: "../assets/brook.png" },
+    { name: "chibi-sanji", image: "../assets/chibi-sanji.png" },
+    { name: "chibi-usopp", image: "../assets/chibi-usopp.png" },
+    { name: "chibi-zoro", image: "../assets/chibi-zoro.png" },
+    { name: "chopper", image: "../assets/chopper.png" },
+    { name: "gear4", image: "../assets/gear4.png" },
+    { name: "kid-luffy", image: "../assets/kid-luffy.png" },
+    { name: "luffy", image: "../assets/luffy.png" },
+    { name: "robin", image: "../assets/robin.png" },
+    { name: "sanji", image: "../assets/sanji.png" },
+    { name: "usopp", image: "../assets/usopp.png" },
+    { name: "zoro", image: "../assets/zoro.png" }
 ]
 
 
